8a55e728609653293ad28e2f62813627e767916f
Xueli Hua <150328932+Xueli-Hua@users.noreply.github.com>
2024-03-25 11:38:01 +0800

    Update EfficiencyMu.C

--- JetEfficiency/src/EfficiencyMu.C
+++ JetEfficiency/src/EfficiencyMu.C
@@ -69,25 +69,25 @@ int Efficiency(char const* input) {
     vector<string> files;
     GetFiles(input, files);
 
-    /* read in calo jet information */
-    TChain offChain("akCs4PFJetAnalyzer/t");
+    /* read in reco mu information */
+    TChain offChain("muonAnalyzer/MuonTree");
     FillChain(offChain, files);
     TTreeReader offReader(&offChain);
-    TTreeReaderValue<int>   jetN(offReader, "ncalo");
-    TTreeReaderArray<float> jetPt(offReader, "calopt");
-    TTreeReaderArray<float> jetEta(offReader, "caloeta");
-    TTreeReaderArray<float> jetPhi(offReader, "calophi");
+    TTreeReaderValue<int>   muN(offReader, "nReco");
+    TTreeReaderArray<float> muPt(offReader, "recoPt");
+    TTreeReaderArray<float> muEta(offReader, "recoEta");
+    TTreeReaderArray<float> muPhi(offReader, "recoPhi");
 
-    /* read in emulated jet information */
+    /* read in emulated mu information */
     TChain emuChain("l1UpgradeEmuTree/L1UpgradeTree");
     FillChain(emuChain, files);
     TTreeReader emuReader(&emuChain);
-    TTreeReaderValue<vector<float>> emuJetPt(emuReader, "jetEt");
-    TTreeReaderValue<vector<float>> emuJetEta(emuReader, "jetEta");
-    TTreeReaderValue<vector<float>> emuJetPhi(emuReader, "jetPhi");
+    TTreeReaderValue<vector<float>> emumuPt(emuReader, "muonEt");
+    TTreeReaderValue<vector<float>> emumuEta(emuReader, "muonEta");
+    TTreeReaderValue<vector<float>> emumuPhi(emuReader, "muonPhi");
 
-    string seed = "L1_SingleJet64_BptxAND";
-    float threshold = 64;
+    string seed = "L1_SingleMu3_BptxAND";
+    float threshold = 3;
 
     /* create histograms for efficiency plots */
     int nbins = 25;
@@ -108,46 +108,46 @@ int Efficiency(char const* input) {
             cout << "Entry: " << i << " / " <<  totalEvents << endl; 
         }
 
-        float maxJetPt = -999;
-        float maxJetPhi = -999;
-        float maxJetEta = -999;
+        float maxmuPt = -999;
+        float maxmuPhi = -999;
+        float maxmuEta = -999;
 
-        float emuMaxJetPt = -999;
-        float emuMatchedJetPt = -999;
+        float emuMaxmuPt = -999;
+        float emuMatchedmuPt = -999;
         float minDR = 10;
 
-        /* iterate through jets and find the jet with max pT */
-        for (int i = 0; i < *jetN; ++i) {
-            if (TMath::Abs(jetEta[i]) > 2) { continue; }
+        /* iterate through muons and find the muon with max pT */
+        for (int i = 0; i < *muN; ++i) {
+            if (TMath::Abs(muEta[i]) > 2) { continue; }
 
-            if (jetPt[i] > maxJetPt) {
-                maxJetPt = jetPt[i];
-                maxJetEta = jetEta[i];
-                maxJetPhi = jetPhi[i];
+            if (muPt[i] > maxmuPt) {
+                maxmuPt = muPt[i];
+                maxmuEta = muEta[i];
+                maxmuPhi = muPhi[i];
             }
         }
 
-        if (maxJetPt > 0) {
-            recoHist.Fill(maxJetPt);
+        if (maxmuPt > 0) {
+            recoHist.Fill(maxmuPt);
 
-            /* iterate through emu jets and find matched and unmatched jets with max pT */
-            for (size_t i = 0; i < (*emuJetPt).size(); ++i) {
-                if ((*emuJetPt)[i] > emuMaxJetPt) {
-                    emuMaxJetPt = (*emuJetPt)[i];
+            /* iterate through emu mus and find matched and unmatched mus with max pT */
+            for (size_t i = 0; i < (*emumuPt).size(); ++i) {
+                if ((*emumuPt)[i] > emuMaxmuPt) {
+                    emuMaxmuPt = (*emumuPt)[i];
                 }
 
-                if (dr((*emuJetEta)[i], (*emuJetPhi)[i], maxJetEta, maxJetPhi) < minDR) {
-                    minDR = dr((*emuJetEta)[i], (*emuJetPhi)[i], maxJetEta, maxJetPhi);
-                    emuMatchedJetPt = (*emuJetPt)[i];
+                if (dr((*emumuEta)[i], (*emumuPhi)[i], maxmuEta, maxmuPhi) < minDR) {
+                    minDR = dr((*emumuEta)[i], (*emumuPhi)[i], maxmuEta, maxmuPhi);
+                    emuMatchedmuPt = (*emumuPt)[i];
                 }
             }
 
-            if (emuMaxJetPt >= threshold) {
-                emuHist.Fill(maxJetPt);
+            if (emuMaxmuPt >= threshold) {
+                emuHist.Fill(maxmuPt);
             }
 
-            if (emuMatchedJetPt >= threshold && minDR < 0.4) {
-                emuMatchedHist.Fill(maxJetPt);
+            if (emuMatchedmuPt >= threshold && minDR < 0.4) {
+                emuMatchedHist.Fill(maxmuPt);
             }
         }
     }
@@ -155,11 +155,11 @@ int Efficiency(char const* input) {
     TGraphAsymmErrors emuRecoEff(&emuHist, &recoHist, "cl=0.683 b(1,1) mode");
     TGraphAsymmErrors emuRecoMatchedEff(&emuMatchedHist, &recoHist, "cl=0.683 b(1,1) mode");
 
-    /* plot the turn ons vs reco jet pt */
+    /* plot the turn ons vs reco mu pt */
     TCanvas recoCanvas("recoCanvas", "", 0, 0, 500, 500);
     recoCanvas.cd();
 
-    emuRecoMatchedEff.GetXaxis()->SetTitle("Reco Jet pT (GeV)");
+    emuRecoMatchedEff.GetXaxis()->SetTitle("Reco #mu pT (GeV)");
     emuRecoMatchedEff.GetXaxis()->CenterTitle(true);
     emuRecoMatchedEff.GetYaxis()->SetTitle("Efficiency");
     emuRecoMatchedEff.GetYaxis()->CenterTitle(true);
@@ -183,10 +183,10 @@ int Efficiency(char const* input) {
     recoLegend.AddEntry(&emuRecoMatchedEff, "#DeltaR Matched", "lep");
     recoLegend.Draw();
 
-    recoCanvas.SaveAs("CaloJetEfficiency.pdf");
+    recoCanvas.SaveAs("RecomuEfficiency.pdf");
 
     /* save histograms to file so I can look at them */
-    TFile* fout = new TFile("histograms.root", "recreate");
+    TFile* fout = new TFile("muhistograms.root", "recreate");
 
     emuHist.Write();
     emuMatchedHist.Write();
