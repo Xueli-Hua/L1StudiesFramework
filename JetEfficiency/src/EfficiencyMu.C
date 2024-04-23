687b4050e1a4de70c5ef33281e84bcffad3eb10e
Xueli Hua <150328932+Xueli-Hua@users.noreply.github.com>
2024-04-23 15:19:46 +0800

    Update EfficiencyMu.C

--- JetEfficiency/src/EfficiencyMu.C
+++ JetEfficiency/src/EfficiencyMu.C
@@ -150,8 +150,8 @@ int Efficiency(char const* input) {
     }
 
     string seed = "L1_SingleMuonOpen_NotMinimumBiasHF2_AND_BptxAND";
-    if (SeedBit.find(seed.c_str()) == SeedBit.end()) { cout << "error" << end; }
-    if (SeedBit[seed.c_str()]>=m_algoDecisionInitial.GetSize()) { cout << "error" << end; }   
+    if (SeedBit.find(seed.c_str()) == SeedBit.end()) { cout << "error" << endl; }
+    if (SeedBit[seed.c_str()]>=m_algoDecisionInitial.GetSize()) { cout << "error" << endl; }   
     bool l1uGTdecision;
 
     /* create histograms for efficiency plots */
